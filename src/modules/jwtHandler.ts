f3173a2b96b0e294a5a2755460848f1285924824
unknown <dragontaek98@naver.com>
2023-01-03 15:06:04 +0900

    add: jwt modules #2

---
+++ src/modules/jwtHandler.ts
@@ -0,0 +1,34 @@
+import jwt from "jsonwebtoken";
+import { tokenType } from "../constants";
+
+const sign = (userId: number) => {
+    const payload = {
+        userId,
+    };
+
+    const accessToken = jwt.sign(payload, process.env.JWT_SECRET as string, { expiresIn: "2h" });
+    return accessToken;
+};
+
+const verify = (token: string) => {
+    let decoded: string | jwt.JwtPayload;
+
+    try {
+        decoded = jwt.verify(token, process.env.JWT_SECRET as string);
+    } catch (error: any) {
+        if (error.message === "jwt expired") {
+            return tokenType.TOKEN_EXPIRED;
+        } else if (error.message === "invalid token") {
+            return tokenType.TOKEN_INVALID;
+        } else {
+            return tokenType.TOKEN_INVALID;
+        }
+    }
+
+    return decoded;
+};
+
+export default {
+    sign,
+    verify,
+};
