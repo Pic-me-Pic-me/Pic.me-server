6e719491309771e13a010f3b096cce3f12127fd4
unknown <dragontaek98@naver.com>
2023-01-04 23:21:00 +0900

    docs: README update

--- src/router/authRouter.ts
+++ src/router/authRouter.ts
@@ -1,28 +1,18 @@
 import { Router } from "express";
 import { authController } from "../controller";
-import { body } from "express-validator"
+import { body } from "express-validator";
 
 const router: Router = Router();
 
 router.post(
     "/",
-    [
-        body("username").notEmpty(),
-        body("email").notEmpty(), 
-        body("email").isEmail(),
-        body("password").isLength({ min: 10 })
-    ],
+    [body("username").notEmpty(), body("email").notEmpty(), body("email").isEmail(), body("password").isLength({ min: 10 })],
     authController.createUser
 );
 
 router.post(
     "/signin",
-    [
-        body("email").notEmpty(),
-        body("email").isEmail(),
-        body("password").notEmpty(),
-        body("password").isLength({ min: 10 }),
-    ],
+    [body("email").notEmpty(), body("email").isEmail(), body("password").notEmpty(), body("password").isLength({ min: 10 })],
     authController.signInUser
 );
 
