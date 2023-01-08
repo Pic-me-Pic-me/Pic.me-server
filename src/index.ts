c3860d1bfa2f509fa7b0d8efd5971a11a84f289c
unknown <dragontaek98@naver.com>
2023-01-08 15:21:16 +0900

    feat: cors setting #41

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,22 @@
 import express, { NextFunction, Request, Response } from "express";
 import router from "./router";
+import cors from "cors";
 
-const app = express(); 
-const PORT = 3000; 
+const app = express();
+const PORT = 3000;
 
-app.use(express.json()); 
+app.use(cors());
+app.use(express.json());
+
 app.use("/", router);
 app.get("/", (req: Request, res: Response, next: NextFunction) => {
-  res.send("server running");
+    res.send("server running");
 });
 
 app.listen(PORT, () => {
-  console.log(`
+    console.log(`
         #############################################
             🛡️ Server listening on port: ${PORT} 🛡️
         #############################################
     `);
-}); 
+});
