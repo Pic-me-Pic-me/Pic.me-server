e263347c3e66dbb00cacc6819a58609dbcb3649b
yangjiyoung <0112jyoung@naver.com>
2023-01-08 13:46:14 +0900

    feat: 투표 생성 및 삭제에 auth 추가

--- src/router/voteRouter.ts
+++ src/router/voteRouter.ts
@@ -4,14 +4,13 @@ import { upload, auth } from "../middlewares";
 
 const router: Router = Router();
 
-router.post("/:userId", upload.array("file"), voteController.createVote);
+router.post("/:userId", auth, upload.array("file"), voteController.createVote);
 
-router.delete("/:voteId", voteController.deleteVote);
+router.delete("/:voteId", auth, voteController.deleteVote);
 
 router.get("/maker/singleResult/:voteId", voteController.getSingleVote); //라우터 경로 추천좀
 
 router.patch("/close/:voteId", auth, voteController.closeVote);
-
 
 router.get("/getCurrentVote/:userId", voteController.getCurrentVotes); //userId + auth 로직 필요??
 //라우팅 경로 고려
