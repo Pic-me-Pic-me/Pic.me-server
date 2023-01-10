32cedc2c24f74363516bae1ef74f8fdd45d49000
gahee99 <u1879@sookmyung.ac.kr>
2023-01-10 21:18:24 +0900

    change t_time

--- src/modules/jwtHandler.ts
+++ src/modules/jwtHandler.ts
@@ -6,7 +6,7 @@ const sign = (userId: number) => {
         userId,
     };
 
-    const accessToken = jwt.sign(payload, process.env.JWT_SECRET as string, { expiresIn: "2h" });
+    const accessToken = jwt.sign(payload, process.env.JWT_SECRET as string, { expiresIn: "30m" });
     return accessToken;
 };
 
