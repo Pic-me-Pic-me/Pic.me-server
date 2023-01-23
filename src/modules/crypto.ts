02b5c367b2527dac477df65cea4c17b87849bbca
yangjiyoung <0112jyoung@naver.com>
2023-01-24 01:08:36 +0900

    feat: 암호화 적용

---
+++ src/modules/crypto.ts
@@ -0,0 +1,19 @@
+import CryptoJS from "crypto-js";
+const privateKey = process.env.CRYPTO_PRIVATE_KEY;
+
+const encodeVoteId = (voteId: number) => {
+    const encodedId = CryptoJS.AES.encrypt(voteId.toString(), privateKey as string).toString();
+    return encodedId;
+};
+
+const decodeVoteId = (encodedId: string) => {
+    const voteId = CryptoJS.AES.decrypt(encodedId, privateKey as string).toString(
+        CryptoJS.enc.Utf8
+    );
+    return voteId;
+};
+
+export default {
+    encodeVoteId,
+    decodeVoteId,
+};
