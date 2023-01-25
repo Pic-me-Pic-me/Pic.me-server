7444d32023a87be2cd859c8ad546c24643b87c74
yangjiyoung <0112jyoung@naver.com>
2023-01-25 20:15:16 +0900

    feat: 코드 리뷰 반영

--- src/modules/crypto.ts
+++ src/modules/crypto.ts
@@ -2,16 +2,13 @@ import CryptoJS from "crypto-js";
 const privateKey = process.env.CRYPTO_PRIVATE_KEY;
 
 const encodeVoteId = (voteId: number) => {
-    let encodedId = "";
-    do {
-        encodedId = CryptoJS.AES.encrypt(voteId.toString(), privateKey as string).toString();
-    } while (encodedId.search("/") != -1);
-
-    return encodedId;
+    const encodedId = CryptoJS.AES.encrypt(voteId.toString(), privateKey as string).toString();
+    return encodeURIComponent(encodedId.toString());
 };
 
 const decodeVoteId = (encodedId: string) => {
-    const voteId = CryptoJS.AES.decrypt(encodedId, privateKey as string).toString(
+    const decodedId = decodeURIComponent(encodedId);
+    const voteId = CryptoJS.AES.decrypt(decodedId, privateKey as string).toString(
         CryptoJS.enc.Utf8
     );
     return voteId;
