a4ba1a39f2f8f95838f8d967b6bb5462b1ef2916
yongtaeklee <dragontaek98@naver.com>
2023-02-01 02:29:44 +0900

    fix: sudo error

--- scripts/run-after-install.sh
+++ scripts/run-after-install.sh
@@ -8,16 +8,16 @@ REPOSITORY=/home/ubuntu/Pic.me-server
 
 cd $REPOSITORY
 
-pm2 kill
+sudo /usr/bin/pm2 kill
 
-yarn
+sudo /usr/bin/yarn
 
 if [ "$DEPLOYMENT_GROUP_NAME" == "mainDeployGroup" ]
 then
-    yarn run deploy:prod
+    sudo /usr/bin/yarn run deploy:prod
 fi
 
 if [ "$DEPLOYMENT_GROUP_NAME" == "testDeployGroup" ]
 then
-    yarn run deploy:dev
+    sudo /usr/bin/yarn run deploy:dev
 fi
