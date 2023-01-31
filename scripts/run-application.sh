4ccf181512b314b6996d42c2a38c82d74e459fe4
yongtaeklee <dragontaek98@naver.com>
2023-02-01 01:46:56 +0900

    fix: unused run stage

--- scripts/run-application.sh
+++ scripts/run-application.sh
@@ -2,18 +2,16 @@ REPOSITORY=/home/ubuntu/Pic.me-server
 
 cd $REPOSITORY
 
-sudo pm2 kill
+pm2 kill
 
-sudo yarn
-
-sudo cross-env NODE_ENV=$ pm2 start dist
+yarn
 
 if [ "$DEPLOYMENT_GROUP_NAME" == "mainDeployGroup" ]
 then
-    sudo yarn deploy:prod
+    yarn run deploy:prod
 fi
 
 if [ "$DEPLOYMENT_GROUP_NAME" == "testDeployGroup" ]
 then
-    sudo yarn deploy:dev
+    yarn run deploy:dev
 fi
