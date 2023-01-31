14cecf51b26dad4eff3e385b1708d201c5cdcf05
yongtaeklee <dragontaek98@naver.com>
2023-02-01 02:10:17 +0900

    fix: deploy script change

--- scripts/change-permissions.sh
+++
@@ -1,5 +0,0 @@
-cd /home/ubuntu/Pic.me-server
-
-sudo chown -R ubuntu:ubuntu /home/ubuntu/Pic.me-server
-chmod 777 /home/ubuntu/Pic.me-server
-chmod 777 /home/ubuntu/Pic.me-server/*/**
--- scripts/run-after-install.sh
+++ scripts/run-after-install.sh
@@ -1,10 +1,16 @@
+cd /home/ubuntu/Pic.me-server
+
+sudo chown -R ubuntu:ubuntu /home/ubuntu/Pic.me-server
+chmod 777 /home/ubuntu/Pic.me-server
+chmod 777 /home/ubuntu/Pic.me-server/*/**
+
 REPOSITORY=/home/ubuntu/Pic.me-server
 
 cd $REPOSITORY
 
 pm2 kill
 
-yarn
+yarn install
 
 if [ "$DEPLOYMENT_GROUP_NAME" == "mainDeployGroup" ]
 then
