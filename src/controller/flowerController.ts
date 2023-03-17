4232d06726e3a3a441283d7a05d5f85259dce50a
gahee99 <u1879@sookmyung.ac.kr>
2023-03-17 22:20:24 +0900

    feat:필요 없는 코드 정리 #86

--- src/controller/flowerController.ts
+++ src/controller/flowerController.ts
@@ -22,58 +22,7 @@ const getSingleFlowerVote = async (req: Request, res: Response, next: NextFuncti
 };
 
 /**
- * get single flower vote result in library
- *
- * @api {get} /flower/current/:voteId
- */
-const getCurrentFlowerVote = async (req: Request, res: Response, next: NextFunction) => {
-    const { voteId } = req.params;
-
-    try {
-        const data = await flowerService.getCurrentFlowerVote(voteId);
-
-        return res.status(sc.OK).send(success(sc.OK, rm.PLAYER_GET_VOTE_SUCCESS, data));
-    } catch (e) {
-        return next(e);
-    }
-};
-
-/**
- * get single flower vote result in library
- *
- * @api {get} /flower/result/:voteId
- */
-const getCurrentFlowerVoteResult = async (req: Request, res: Response, next: NextFunction) => {
-    const { voteId } = req.params;
-
-    try {
-        const data = await flowerService.getCurrentFlowerVoteResult(voteId);
-
-        return res.status(sc.OK).send(success(sc.OK, rm.PLAYER_GET_VOTE_SUCCESS, data));
-    } catch (e) {
-        return next(e);
-    }
-};
-
-/**
- * get single flower vote result in library
- *
- * @api {get} /flower/common/:pictureId
- */
-const playerGetFlowerVotedResult = async (req: Request, res: Response, next: NextFunction) => {
-    const { voteId } = req.params;
-
-    try {
-        const data = await flowerService.playerGetFlowerVotedResult(voteId);
-
-        return res.status(sc.OK).send(success(sc.OK, rm.PLAYER_GET_VOTE_SUCCESS, data));
-    } catch (e) {
-        return next(e);
-    }
-};
-
-/**
- * get single flower vote result in library
+ * [Player] get flower vote Picture
  *
  * @api {get} /flower/common/:pictureId
  */
@@ -91,9 +40,6 @@ const playerGetFlowerVotePicture = async (req: Request, res: Response, next: Nex
 
 const flowerController = {
     getSingleFlowerVote,
-    getCurrentFlowerVote,
-    getCurrentFlowerVoteResult,
-    playerGetFlowerVotedResult,
     playerGetFlowerVotePicture,
 };
 
--- src/router/flowerRouter.ts
+++ src/router/flowerRouter.ts
@@ -6,13 +6,6 @@ const router: Router = Router();
 // get single flower vote result in library
 router.get("/library/:voteId", flowerController.getSingleFlowerVote);
 
-//용택리..위에 라우터 겹칠 것 같은데
-// get current single flower vote
-router.get("/current/:voteId", flowerController.getCurrentFlowerVote);
-
-// get current single flower vote result
-router.get("/result/:voteId", flowerController.getCurrentFlowerVoteResult);
-
 /*
  플레이어
 */
