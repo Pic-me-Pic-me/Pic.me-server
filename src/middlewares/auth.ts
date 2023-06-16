77493e43b5bd448812a310fba000f6f821982177
dragontaek-lee <dragontaek98@naver.com>
2023-06-17 03:55:57 +0900

    fix: notification auth skip #89

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -5,7 +5,7 @@ import { fail } from "../constants/response";
 import tokenType from "../constants/tokenType";
 import jwtHandler from "../modules/jwtHandler";
 
-const skipAuthPath = ["/sticker", "/auth", "/user/name", "/user/email", "/vote/common"];
+const skipAuthPath = ["/sticker", "/auth", "/user/name", "/user/email", "/vote/common", "/alarm"];
 const uploadPath = "/vote";
 
 export default async (req: Request, res: Response, next: NextFunction) => {
